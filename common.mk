9968c2892cf7e0a53bb934fdd47a058bbf3e7fb3
Akash <akashniki@gmail.com>
2019-05-29 06:47:05 +0000

    sdm845-common:Use Pixel 3's msm_irqbalance.conf

--- common.mk
+++ common.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
 
+#MSM_Irqbalance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/etc/msm_irqbalance.conf
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
