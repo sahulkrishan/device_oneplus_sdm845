AOSiP-Devices/device_oneplus_sdm845-common

sdm845-common: rootdir: Add init script to create and modify swap
Co-authored-by: Zachariah Kennedy <zkennedy87@gmail.com>
Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Change-Id: I99f216a23e49adb4ee8b32f375a6f41b72ecba81

common.mk
@@ -66,7 +66,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.recovery.qcom.rc \
-    init.target.rc
+    init.target.rc \
+    init.swap.sh
 
 # Display
 PRODUCT_PACKAGES += \

sahulkrishan/device_oneplus_sdm845

common.mk
@@ -62,7 +62,8 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.recovery.qcom.rc \
-    init.prelude.rc
+    init.prelude.rc \
+    init.swap.sh
 
 # Display
 PRODUCT_PACKAGES += \